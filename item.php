4b6f90a9cbfd26d194f2119c40750199b2e359ed
Yann Cheyrezy <VotreMail@Mail>
2022-04-29 15:07:45 +0200

    changement sur item.php

--- item.php
+++ item.php
@@ -1,1 +1,9 @@
-<?php
+<html>
+<body>
+<?php echo 'Claquette';
+echo '150€';
+echo("<img src=\"https://i.f1g.fr/media/madame/1900x1900_crop/sites/default/files/img/2020/04/claquette-adilette.jpg\" width=\"500\" height=\"500\"/>
+");
+?>
+</body>
+</html>
