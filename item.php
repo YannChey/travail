e1a169d3da59a5fef907c72915f91dd27e7f9e2b
Yann Cheyrezy <VotreMail@Mail>
2022-05-02 16:56:58 +0200

    Modified item.php

--- item.php
+++ item.php
@@ -1,16 +1,17 @@
 
 <?php
 include 'header.php';
-$Name = "claquette";
+$name = "claquette";
 $price = "150€";
-$image = "https://i.f1g.fr/media/madame/1900x1900_crop/sites/default/files/img/2020/04/claquette-adilette.jpg";
-
-echo $Name;
-echo "<br>";
-echo $price;
-echo "<br>";
+$img = "https://i.f1g.fr/media/madame/1900x1900_crop/sites/default/files/img/2020/04/claquette-adilette.jpg";
+echo $name;
 ?>
-<img src="<?php echo $image ?>"  />
+<br>
+<?php
+echo $price;
+?>
+<br>
+<img src="<?php echo $img ?>"  />
 <?php
 include 'footer.php';
 
